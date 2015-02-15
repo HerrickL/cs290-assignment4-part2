c9dc94062df5245c0935b79c33e2cb1e60605dc6
Erik Ratcliffe <erat@erat-PC.(none)>
2015-02-15 15:54:00 -0800

    Functionality seems to work. Now, time to clean up the fragmented code.

--- src/editRow.php
+++ src/editRow.php
@@ -3,23 +3,33 @@ error_reporting ( E_ALL );
 ini_set ( 'display_errors', 'On' );
 
 if ($_POST) {
+	print_r($_POST);
 	$db = new mysqli ( "localhost", "root", "root", "inventory_db" );
 	if ($db->connect_errno) {
 		echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
 	}
-	if($_POST['delete']) {
+	// If we're deleting a video...
+	if(isset($_POST['delete'])) {
 	   	$inId = $_POST ['delete'];
 		echo "<h1>The id that will be deleted is " . $inId . "</h1>";
 				if (! ($db->query ( "DELETE FROM inventory WHERE id={$inId}" ))) {
 			echo "Delete failed: (" . $db->errno . ") " . $db->error;
 		}
 	}
-	if($_POST['chkInOut']) {
+	// If we're checking in/out a video...
+	if(isset($_POST['chkInOut'])) {
 		$inId = $_POST['chkInOut'];
 		if (! ($vidStat = $db->query ( "UPDATE inventory SET rented = !rented WHERE id={$inId}" ))) {
 			echo "Update failed: (" . $db->errno . ") " . $db->error;
 		}
 	}
+	// If we're clearing out all videos...
+	if(isset($_POST['clearOut'])) {
+		if (! $db->query ( "TRUNCATE TABLE inventory" )) {
+			echo "Truncation failed: (" . $db->errno . ") " . $db->error;
+		}
+	}
+
 }
 
 $path = explode('/', $_SERVER['PHP_SELF'], - 1);
--- src/index.php
+++ src/index.php
@@ -1,6 +1,58 @@
 <?php
 error_reporting ( E_ALL );
 ini_set ( 'display_errors', 'On' );
+
+// Connect to the database...
+$db = new mysqli ( "localhost", "root", "root", "inventory_db" );
+if ($db->connect_errno) {
+	echo "ERROR: Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+}
+
+// If we received form data, process it first.
+if($_POST) {
+    if(isset($_POST['name']) && ($_POST['name'] != NULL)) {
+        $validated = TRUE;
+        $inName = $_POST ['name'];
+        if((isset($_POST['category'])) && ($_POST['category'] != NULL)) {
+            $inCat = $_POST ['category'];
+        } else {
+            $inCat = "[Uncategorized]";
+        }
+        if((isset($_POST['minutes']) && ($_POST['minutes'] != NULL))) {
+                if (( string ) ( int ) $_POST ['minutes'] === ( string ) $_POST ['minutes']) {
+                    // Check that the number of minutes is is >= 0
+                    if (( int ) $_POST ['minutes'] >= 0) {
+                        $inLength = $_POST ['minutes'];
+                    } else {
+                        echo "<p>ERROR: Video length must be at least 0.</p>";
+                        $validated = FALSE;
+                    }
+                } else {
+                    echo "<p>ERROR: Video length must be an integer.</p>";
+                    $validated = FALSE;
+                }
+        } else {
+        	$inLength = NULL;
+        }
+                
+        // At this point, all values are validated.
+        if ($validated === TRUE) {
+            if (! ($stmt = $db->prepare ( "INSERT INTO inventory(name,category,length) VALUES (?,?,?)" ))) {
+                echo "ERROR: Prepare failed: (" . $db->errno . ") " . $db->error;
+            }
+            if (! $stmt->bind_param ( "ssi", $inName, $inCat, $inLength )) {
+                echo "ERROR: Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+            }
+            if (! $stmt->execute ()) {
+                echo "ERROR: Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+            }
+	   		$stmt->close ();
+        }
+    } elseif((isset($_POST['category']) || (isset($_POST['minutes'])))) {
+    	echo "<p>ERROR: The name field is required when adding videos.</p>\n";
+    }
+}
+		
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,62 +68,55 @@ ini_set ( 'display_errors', 'On' );
 		</label> <label>Length (minutes): <input type="number" name="minutes"></label>
 		<input type="submit" value="Add Video">
 	</form>
+	
+	<form action="editRow.php" method="post" name="tableFunc">
+		<button type="submit" name="clearOut" value="deleteAllVids">Delete all videos</button>
+	</form>
 
-<?php
-// If we received form data, process it first.
-if ($_POST) {
-	
-	$validated = TRUE;
-	
-	$inName = $_POST ['name'];
-	$inCat = $_POST ['category'];
-	
-	if (( string ) ( int ) $_POST ['minutes'] === ( string ) $_POST ['minutes']) {
-		// Check that the number of minutes is is >= 0
-		if (( int ) $_POST ['minutes'] >= 0) {
-			$inLength = $_POST ['minutes'];
-		} else {
-			echo "<p>Video length must be at least 0.</p>";
-			$validated = FALSE;
-		}
-	} else {
-		echo "<p>Video length must be an integer.</p>";
-		$validated = FALSE;
-	}
-	
-	// At this point, all values are validated.
-	if ($validated === TRUE) {
-		$db = new mysqli ( "localhost", "root", "root", "inventory_db" );
-		if ($db->connect_errno) {
-			echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
-		}
-		
-		if (! ($stmt = $db->prepare ( "INSERT INTO inventory(name,category,length) VALUES (?,?,?)" ))) {
-			echo "Prepare failed: (" . $db->errno . ") " . $db->error;
-		}
-		
-		if (! $stmt->bind_param ( "ssi", $inName, $inCat, $inLength )) {
-			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-		}
-		
-		if (! $stmt->execute ()) {
-			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-		}
-		
-		$stmt->close ();
-	}
-}
-
-// Populate the table with data from the database
-$db = new mysqli ( "localhost", "root", "root", "inventory_db" );
-if ($db->connect_errno) {
-	echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
-}
-
-if (! ($stmt = $db->prepare ( "SELECT id, name, category, length, rented FROM inventory" ))) {
+	<form action="index.php" method="post" name="tableFilter">
+		<select name="showCategory">
+<?php 
+if (! ($catList = $db->prepare ( "SELECT DISTINCT category FROM inventory ORDER BY category" ))) {
 	echo "Prepare failed: (" . $db->errno . ") " . $db->error;
 }
 
+$inCat = NULL;
+if (! $catList->bind_result ( $inCat )) {
+	echo "Binding output parameters failed: (" . $catList->errno . ") " . $catList->error;
+}
+		
+if (! $catList->execute ()) {
+	echo "Execute failed: (" . $catList->errno . ") " . $catList->error;
+}
+
+// Required for num_rows to work
+$catList->store_result();
+
+// If we got more than 0 results, populate the category menu with categories
+if($catList->num_rows() > 0) {
+	echo "\t\t\t<option selected value=\"allMovies\">All Movies</option>\n";
+	while ( $catList->fetch () ) {
+		echo "\t\t\t<option value=\"{$inCat}\">{$inCat}</option>\n";
+	}
+}
+
+$catList->close();
+?>
+		</select>
+		<input type="submit" value="Filter">
+	</form>
+
+<?php
+
+
+// Add a string to the end of the query that matches a specific category, if necessary
+$queryStr = "SELECT id, name, category, length, rented FROM inventory";
+if(isset($_POST['showCategory']) && ($_POST['showCategory'] !== "allMovies")) {
+	$queryStr .= " WHERE category=\"" . $_POST['showCategory'] . "\"";
+}
+if (! ($stmt = $db->prepare ( $queryStr ))) {
+	echo "Prepare failed: (" . $db->errno . ") " . $db->error;
+}
 if (! $stmt->execute ()) {
 	echo "Execute failed: (" . $db->errno . ") " . $db->error;
 }
